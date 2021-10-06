d004bb1b8c062e321beba012db51d640f757b52b
LisaCee <lisaceedesign@gmail.com>
2021-10-06 15:03:28 -0700

    intro style tweaks

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,30 +3,37 @@ import Logo from '../images/avatar350.jpg';
 
 export default function Intro() {
 	return (
-		<div className="container mt-5 mx-auto px-2">
-      
-      <div className="sm:flex sm:grid-cols-12">
-        <div
-          className="flex-1 text-center bg-gray-400 px-5 py-5 m-2 rounded"
-        >
-          <div className="lg:flex lg:items-center">
-            <div className="lg:flex-shrink-0">
-              <img className="rounded-2xl lg:w-64" src={Logo} alt="" />
-            </div>
-            <div className="mt-4 lg:mt-0 lg:ml-6">
-              <h1
-                className="uppercase tracking-wide text-3xl text-primary font-bold"
-              >
-                Hi. I'm Lisa Cee.
-              </h1>
-              <p> Adaptive | Inquisitive | Compassionate</p>
-			  <p>Passionate full stack developer, creating websites that remove
-              barriers to access and make information available to everyone.</p>
-            </div>
-          </div>
-        </div>
+
+		<div className="container mx-auto">
+
+			<div className="sm:flex sm:grid-cols-12 md:grid-cols-6">
+				<div
+				className="flex-1 text-center bg-light rounded"
+				>
+					<div className="md:flex sm:px-5 xl:px-10">
+						<div className="flex justify-center my-5">
+							<img className="rounded-3xl my-center p-2" src={Logo} alt="" />
+						</div>
+
+						<div className="m-auto">
+						<h1
+						className="-mx-1 tracking-wide text-4xl text-primary font-bold"
+						>
+						Hi. I'm Lisa Canini
+						</h1>
+
+						<h2 className="text-xl mb-5">(aka Lisa Cee)</h2>
+
+						<p className="text-lg mb-5"> Adaptive | Inquisitive | Compassionate</p>
+
+						<p className="px-5 text-md">Passionate full stack developer, creating websites that remove
+						barriers to access and make information available to everyone.</p>
+						</div>
+
+					</div>
+				</div>
+			</div>
+
 		</div>
-		</div>
-		
 	)
 }
