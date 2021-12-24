5f3f90802ebbad7e759762f91c8acad163e5ebf0
LisaCee <lisaceedesign@gmail.com>
2021-12-23 16:44:12 -0800

    homepage layout

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,35 +3,32 @@ import Logo from '../images/avatar350.jpg';
 
 export default function Intro() {
 	return (
+		<div className="bg-gradient home p-5 md:pt-40">
+			<div className="md:flex md:justify-around md:items-center">
+				<div>
+					<h1
+					className="text-5xl text-white font-bold headline md:text-8xl"
+					>Hi, I'm Lisa Cee.</h1>
 
-		<div className="mx-auto lg:px-28 bg-gradient">
-
-			<div className="mx-auto sm:grid-cols-12 md:grid-cols-6">
-				<div
-				className="w-full text-center"
-				>
-					<div className="md:flex sm:px-5 xl:px-10">
-						<div className="flex justify-center">
-							<img className="rounded-3xl my-center p-2" src={Logo} alt="" />
-						</div>
-
-						<div className="m-auto">
-						<h1
-						className="-mx-1 tracking-wide text-4xl text-gray-800 font-bold pb-5"
-						>
-						Hi. I'm Lisa Canini
-						</h1>
-
-						<p className="text-lg text-gray-800 mb-5"> I am... <br /> Adaptive | Inquisitive | Compassionate</p>
-
-						<p className="px-5 pb-5 text-gray-800text-md">Passionate full stack developer, creating websites that remove
-						barriers to access and make information available to everyone.</p>
-						</div>
-
-					</div>
+					<p className="text-white text-2xl">And I like to make things better.</p>
+				</div>
+				<div className="md:pt-150">
+					<img className="rounded-full" src={Logo} alt="" />
 				</div>
 			</div>
-
 		</div>
 	)
 }
+
+// import React from "react";
+// import Logo from '../images/avatar350.jpg';
+
+// export default function Intro() {
+
+// 	return (
+// 		<div className="bg-gradient home">
+// 			<h1 className="-mx-1 tracking-wide text-6xl text-white font-bold pb-5 headline">Hi, I'm Lisa Cee.</h1>
+// 			<img className="rounded-3xl my-center p-2" src={Logo} alt="" />
+// 		</div>
+// 		);
+// }
